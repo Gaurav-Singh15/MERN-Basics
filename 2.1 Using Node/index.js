5a6f3c5263ce856c4cb9edfca2c2efb9aa0af98a
Gaurav Singh <122213065+Gaurav-Singh15@users.noreply.github.com>
2024-04-09 03:38:39 +0530

    Add files via upload

---
+++ 2.1 Using Node/index.js
@@ -0,0 +1,9 @@
+import generateName from 'sillyname'
+import superheroes from 'superheroes';
+
+
+var sname = superheroes.random();
+
+var sillyname = generateName();
+
+console.log(`I am ${sname}`);
